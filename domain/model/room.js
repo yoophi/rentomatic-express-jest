363b024189600db68d22899298778dd047cc04d1
Pyunghyuk Yoo <yoophi@gmail.com>
2021-10-27 23:28:53 +0900

    :sparkles: NEW: init Room model

---
+++ domain/model/room.js
@@ -0,0 +1,10 @@
+class Room {
+  constructor({ code, size, price, longitude, latitude }) {
+    this.code = code;
+    this.size = size;
+    this.price = price;
+    this.longitude = longitude;
+    this.latitude = latitude;
+  }
+}
+module.exports = Room;
---
+++ domain/model/room.spec.js
@@ -0,0 +1,18 @@
+const { v4: uuid } = require("uuid");
+const Room = require("./room");
+
+test("room should be initialized ", () => {
+  code = uuid();
+  room = new Room({
+    code,
+    size: 200,
+    price: 10,
+    longitude: -0.09998975,
+    latitude: 51.75436293,
+  });
+  expect(room.code).toBe(code);
+  expect(room.size).toBe(200);
+  expect(room.price).toBe(10);
+  expect(room.longitude).toBe(-0.09998975);
+  expect(room.latitude).toBe(51.75436293);
+});
